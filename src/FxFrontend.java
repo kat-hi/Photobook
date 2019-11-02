456c6130304d6967f99b820008e53f81cd84cb9d
kat-ho <s77518@beuth-hochschule.de>
2019-11-02 16:22:13 +0100

    lambda and photowall with a chosen directory

--- src/FxFrontend.java
+++ src/FxFrontend.java
@@ -1,18 +1,14 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.image.Image;
-
-import java.io.IOException;
+import java.io.File;
 
 public class FxFrontend extends Application {
     public static void main(String[] args) {
@@ -22,21 +18,51 @@ public class FxFrontend extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("Hello World!");
-
+        stage.setResizable(true);
         GridPane root = new GridPane();
-        Button openBtn = new Button("open File");
+        Button fileBtn = new Button("open File");
+        Button directoryBtn = new Button("open Directory");
 
-        root.add(openBtn, 0, 0);
+        root.add(fileBtn, 0, 0);
+        root.add(directoryBtn, 0, 1 );
+        root.setPadding(new Insets(10, 10, 10, 10));
+        root.setHgap(10);
+        root.setVgap(10);
 
-        openBtn.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                FileChooser fileChooser = new FileChooser();
-                fileChooser.setTitle("Open Resource File");
-                String filepath = fileChooser.showOpenDialog(stage).getPath();
-                System.out.println(filepath);
-                if(!filepath.equals("")){
-                    Image image = new Image("file:"+filepath, 700, 800, true, true,true);
-                    root.add(new ImageView(image), 3,1);
+        fileBtn.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            String filepath = fileChooser.showOpenDialog(stage).getPath();
+            System.out.println(filepath);
+            if(!filepath.equals("")){
+                Image image = new Image("file:"+filepath, 500, 500, true, true,true);
+                root.add(new ImageView(image), 3,1);
+            }
+        });
+
+        //directoryBtn.setOnAction(new EventHandler<ActionEvent>() {
+        //    @Override
+        //    public void handle(ActionEvent event) {
+
+        directoryBtn.setOnAction(event -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            directoryChooser.setTitle("load directory");
+            File selectedDirectory = directoryChooser.showDialog(stage);
+            File[] fileList = selectedDirectory.listFiles();
+            if (fileList != null) {
+                int colIndex = 0;
+                int rowIndex = 0;
+
+                for (File filepath : fileList) {
+                    System.out.println(filepath);
+                    Image image = new Image("file:"+filepath, 200,200,true,true,true);
+                    root.add(new ImageView(image), colIndex, rowIndex);
+
+                    colIndex+=1;
+                    if(colIndex == 5) {
+                        rowIndex+=1;
+                        colIndex = 0;
+                    }
                 }
             }
         });
