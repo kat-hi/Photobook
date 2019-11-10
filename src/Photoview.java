7a42f41ddf62659bbe3a2a8870ba7c8e0799b93f
kat-ho <s77518@beuth-hochschule.de>
2019-11-10 22:00:32 +0100

    new methods: addImage and addImages

--- src/Photoview.java
+++ src/Photoview.java
@@ -1,20 +1,45 @@
 import javafx.geometry.Pos;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
+import java.io.File;
+
 public class Photoview extends GridPane {
 
-    private BorderPane borderPane;
-
-    public Photoview(BorderPane root, Pos position) {
-        root.getChildren().add(this);
-        this.setAlignment(position);
+    public Photoview () {
     }
 
-
-    public void add(ImageView imageView, int colIndex, int rowIndex) {
-
+    public Photoview(BorderPane root) {
+        System.out.println("Create Photoview");
+        GridPane grid = new GridPane();
+        root.getChildren().add(grid);
+        root.setCenter(this);
     }
 
+    public void addImage(int colIndex, int rowIndex, String filepath) {
+        Image image = new Image("file:"+filepath, 200,200,true,true,true);
+        this.add(new ImageView(image), colIndex, rowIndex);
+    }
+
+    public void addImages(File directoryPath) {
+        File[] fileList = directoryPath.listFiles();
+        if (fileList != null) {
+            int colIndex = 0;
+            int rowIndex = 0;
+
+            for (File file : fileList) {
+                System.out.println(file.getPath());
+                Image image = new Image("file:"+file.getPath(), 200,200,true,true,true);
+                this.add(new ImageView(image), colIndex, rowIndex);
+
+                colIndex+=1;
+                if(colIndex == 5) {
+                    rowIndex+=1;
+                    colIndex = 0;
+                }
+            }
+        }
+    }
 }
