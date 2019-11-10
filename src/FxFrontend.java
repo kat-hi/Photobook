a6450b471fdbb1b902a435a2a5dccae38d33bc85
kat-ho <s77518@beuth-hochschule.de>
2019-11-10 21:59:43 +0100

    new constructor parameter

--- src/FxFrontend.java
+++ src/FxFrontend.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class FxFrontend extends Application {
+    public static Photoview photoview;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -12,21 +15,19 @@ public class FxFrontend extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("Photobook!");
-        stage.setResizable(true);
-
+        stage.setMaximized(true);
         BorderPane root = new BorderPane();
-        root.setSpacing(5);
-        root.setPadding(new Insets(5, 10 ,5, 10));
+        // root.setSpacing(5);
+        // root.getChildren().setPadding(new Insets(5, 10 ,5, 10));
 
         Scene scene = new Scene(root, 800, 600);
         stage.setTitle("Photobook");
         stage.setScene(scene);
 
-        MenuTab menuTab = new MenuTab(root, BASELINE_CENTER);
-        FilterTab filterTab = new FilterTab(root, BASELINE_CENTER);
-        BrowserBar browserBar = new BrowserBar(root, BASELINE_LEFT);
-        Photoview photoview = new Photoview(root, BASELINE_RIGHT);
-
+        MenuTab menuTab = new MenuTab(root, stage);
+        // FilterTab filterTab = new FilterTab(root);
+        // BrowserBar browserBar = new BrowserBar(root);
+        photoview = new Photoview(root);
         stage.show();
     }
 }
